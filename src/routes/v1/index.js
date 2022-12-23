bb70cfed0449ec1b5f2bc6c321330c144b5959c3
Amit Kumar Roy <akumarroy86@gmail.com>
2022-12-23 12:26:36 +0530

    Added middleware to validate incoming request

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+ 
+const { AuthRequestValidator } = require('../../middlewares/index')
 
 const UserController = require('../../controllers/user-controller');
 
-router.route('/signup').post(UserController.create);
+router.route('/signup').post(
+    AuthRequestValidator.validateUserAuth,
+    UserController.create
+    );
+router.route('/signin').post(
+    AuthRequestValidator.validateUserAuth,
+    UserController.signIn
+    );
+
 
 module.exports = router;
